e1334cf6d67cc9ab0f32e33d789cc6cd103bb540
Precious <okolaaprecious@gmail.com>
2022-10-28 14:10:19 +0100

    Ready for deployment

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+
+header('Access-Control-Allow-Methods: GET, POST');
+
+header("Access-Control-Allow-Headers: X-Requested-With");
+if (isset($_GET['slackusername'])) {
+    $slackusername = $_GET['slackusername'];
+    $backend = $_GET['backend'];
+    $age = $_GET['age'];
+    $bio = $_GET['bio'];
+} else {
+    $slackusername = "Blaze";
+    $backend = "true";
+    $age = "30";
+    $bio = "PHP backend developer with Networking and  Cybersec experience";
+}
+
+settype($slackusername, "string");
+
+settype($age, "integer");
+settype($bio, "string");
+
+
+$myArr = array("slackUsername : $slackusername", "backend : $backend", "Age : $age", "Bio : $bio");
+
+$myJSON = json_encode($myArr);
+
+echo $myJSON;
+?>
